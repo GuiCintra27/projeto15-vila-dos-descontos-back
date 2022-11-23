2954422830bfa840359a9f79103028b77a6075dc
Cintra <gui7lk@gmail.com>
2022-11-23 10:05:04 -0300

    initial commit

---
+++ src/database/db.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import {MongoClient} from "mongodb";
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+
+try{
+    await mongoClient.connect();
+}catch(err){
+    console.log(err);
+}
+
+const db =  mongoClient.db("vila-dos-descontos-db");
+export const salles = db.collection("salles");
+export const products = db.collection("products");
+export const users = db.collection("users");
+export const sessions = db.collection("sessions");
